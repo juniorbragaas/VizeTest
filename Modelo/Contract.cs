7e5925d56c633b7ad0a4c52be3d724a87d037c09
JUNIOR <JUNIOR@DESKTOP-C6QK4B6>
2025-05-27 13:45:54 -0300

    Gerando modelos de Classes

---
+++ Modelo/Contract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Contract
+    {
+        public string ContractId { get; set; }
+        public string ClientId { get; set; }
+        public string ClientName { get; set; }
+        public List<Installment> Installments { get; set; }
+    }
+}
---
+++ Modelo/Installment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Installment
+    {
+        public string InstallmentId { get; set; }
+        public DateTime DueDate { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+        public bool Anticipated { get; set; }
+    }
+}
