c70653a6321a8d72005ab325bb94b32fce66fcbb
JUNIOR <JUNIOR@DESKTOP-C6QK4B6>
2025-05-27 12:58:06 -0300

    CRiando Layout Projeto

--- Negocio/AdiantamentoBO.cs
+++ Negocio/AdiantamentoBO.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Modelo
+namespace Negocio
 {
-    public class Class1
+    internal class AdiantamentoBO
     {
     }
 }
